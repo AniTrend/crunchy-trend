e30af17842a2a8fcfeb8461931bc5fb67d685c72
Maxwell <mxt.developer@gmail.com>
2020-12-15 16:53:37 +0200

    Update support-arch-* dependency to latest experimental

--- buildSrc/src/main/java/co/anitrend/support/crunchyroll/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/support/crunchyroll/buildSrc/Libraries.kt
@@ -287,7 +287,7 @@ object Libraries {
     object AniTrend {
 
         object Arch {
-			private const val version = "0770fc6ace"
+			private const val version = "f998eca6ae"
             const val ui = "com.github.anitrend.support-arch:support-ui:${version}"
             const val ext = "com.github.anitrend.support-arch:support-ext:${version}"
             const val core = "com.github.anitrend.support-arch:support-core:${version}"
